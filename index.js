cc3794489d78451a72d08b5120df6a1006b7562d
Heather Booker <heather.booker@hotmail.com>
2017-01-04 01:20:43 -0500

    add no-cors mode to http request: :

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function getCurrentScore(userToFind) {
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
       'Cache-Control': 'no-cache',
       'Connection': 'keep-alive',
+      'Mode': 'no-cors',
       'Cookie': 'lang=en; duo_ab=f1854cb2e7a39e6ef98065a62f3420a4daeb2679eyJzY2hvb2xzX25ld19zaWdudXBfdGVzdCI6IHRydWUsICJ3ZWJfYnJhbmRlZF9tb2JpbGVfYnV0dG9uc190ZXN0IjogZmFsc2UsICJzaWdudXBfbW9kYWxfYnV0dG9uc19leHBlcmltZW50IjogZmFsc2UsICJsb2dpbl9tb2RhbF9leHBlcmltZW50IjogZmFsc2V9; _ga=GA1.2.81721699.1483480535; auth_tkt=c46285b7fb02fae63a267e82c0dd7636586c382028418880!userid_type:int; wuuid=1563278f-f14c-4697-8c26-8502a2c9aeae; AWSELB=91E12D7D1E1B37CA82C3043817CF6AE0B5D25E6E40C00F8DD3EA43A0D7AD4EE7503FA6238BC7ED8745C965B525095F6B5189C9649CB5F401684F98C2E69E62DB7B301FCFC4; mp_mixpanel__c=0; __utma=226518408.81721699.1483480535.1483487466.1483500381.4; __utmc=226518408; __utmz=226518408.1483480565.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
   };
 
