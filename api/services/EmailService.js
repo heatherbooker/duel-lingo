b226f5a49ae96af8bcd866d204bb5024277245d1
Heather Booker <heather.booker@hotmail.com>
2017-01-30 13:47:15 -0500

    Add node-scheduler to manage email reminders

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const mailgun = require('nodemailer-mailgun-transport');
+const cron = require('node-schedule');
 
 
 function sendEmail(recepient, opponent, duelId) {
@@ -19,13 +20,37 @@ function sendEmail(recepient, opponent, duelId) {
   };
 
   transporter.sendMail(mailOptions, function(err, info){
-    if(err){
+    if (err) {
       return console.log(err);
     }
     console.log('Message sent: ' + JSON.stringify(info));
   });
 }
 
+
+function scheduleEmails(user1, user2, duelId, endDate) {
+  const min = endDate.getMinutes() + 1;
+  const hour = endDate.getHours();
+  const day = endDate.getDay();
+  const month = endDate.getMonth();
+  const year = endDate.getFullYear();
+
+  const date = new Date(year, month, day, hour, min);
+  const job = cron.scheduleJob(date, function() {
+
+    // sendEmail(user1.email, user2.username, duelId);
+    // sendEmail(user2.email, user1.username, duelId);
+    if (user1.email !== null) {
+      console.log('for USER 1: this is the thing! the thing is happening!');
+    }
+    if (user2.email !== null) {
+      console.log('for USER 2: this is the thing! the thing is happening!');
+    }
+    
+  });  
+}
+
+
 module.exports = {
-    sendEmail  
+    scheduleEmails
 };
