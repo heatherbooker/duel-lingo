1d6b91f9a7331d2b2f79e7516e35fa2b025ac35e
Heather Booker <heather.booker@hotmail.com>
2017-02-25 00:14:04 -0800

    Remove DestroyDBService (needed only for development)

--- api/services/DestroyDatabaseService.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  destroyAll: function(areYouSure) {
-    if (areYouSure) {
-      Duel.destroy({}, function() {});
-      User.destroy({}, function() {});
-      console.log('destroyed everything!');
-    }
-  }
-}
